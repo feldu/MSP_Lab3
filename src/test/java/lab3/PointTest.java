497e53fca3dafcdf7cc29838a7707c9d6413bb73
raineduc <hramil0120@gmail.com>
2021-04-20 00:57:16 +0300

    Add tests template

---
+++ src/test/java/lab3/PointTest.java
@@ -0,0 +1,15 @@
+package lab3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PointTest {
+
+    @Test
+    @DisplayName("Test message")
+    public void testTestLol() {
+        assertEquals(1, 2, "failed");
+    }
+}
