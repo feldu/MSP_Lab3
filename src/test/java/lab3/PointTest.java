550184abd01421872ecca2d26e29699e6267d644
Egor Ostryakov <eg.ostr@mail.ru>
2021-04-20 01:54:51 +0300

    блядь а я говорил давай тупа спиздим нет нахуй нужно было выёбываться давай блядь 5 жид юнит подрубим нахуй да пошли вы все в пизду со своими антами нахуй

--- src/test/java/lab3/PointTest.java
+++ src/test/java/lab3/PointTest.java
@@ -1,15 +1,15 @@
 package lab3;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class PointTest {
 
     @Test
-    @DisplayName("Test message")
     public void testTestLol() {
-        assertEquals(1, 2, "failed");
+        assertEquals(1, 2);
     }
 }
